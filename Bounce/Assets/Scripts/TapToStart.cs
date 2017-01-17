f3c7de32acdbb27d743f6848575aeff5a88106a3
Fabio Benigno <fabio.benigno@indt.org.br>
2017-01-17 16:25:55 -0400

    Create simple game menu with music
    Signed-off-by: Fabio Benigno <fabio.benigno@indt.org.br>

---
+++ Bounce/Assets/Scripts/TapToStart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TapToStart : MonoBehaviour {
+    public int nextSceneIndex = 1;
+	
+	void Update ()
+    {
+		if (Input.GetMouseButtonUp(0))
+        {
+            LoadGame();
+        }
+	}
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
