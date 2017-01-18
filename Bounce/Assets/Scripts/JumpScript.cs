406308aae233126306dcf83a95d81a1e5c38f5d3
Fabio Benigno <benigno.fabio@gmail.com>
2017-01-18 17:06:40 -0400

    Add basic player character
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

---
+++ Bounce/Assets/Scripts/JumpScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour {
+    private Rigidbody rigidbody;
+    private bool jump;
+    public int jumpForce;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (jump)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce);
+            Debug.Log("Jump");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0) && !jump)
+        {
+            StartJump();
+        }
+    }
+
+    private void StartJump()
+    {
+        jump = true;
+        Invoke("StopJump", 1);
+    }
+
+    private void StopJump()
+    {
+        jump = false;
+    }
+}
