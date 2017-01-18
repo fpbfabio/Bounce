9977f7832c0df1676725d0deceff007ff9379f53
Fabio Benigno <benigno.fabio@gmail.com>
2017-01-18 14:33:50 -0400

    Create game scene with floor for a infinity run
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

---
+++ Bounce/Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMovement : MonoBehaviour {
+    private Rigidbody rigidbody;
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rigidbody.velocity = speed * Vector3.back;
+    }
+}
---
+++ Bounce/Assets/Scripts/SpawnFloor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFloor : MonoBehaviour {
+    public GameObject prefab;
+    public float x;
+    public float y;
+    public float z;
+
+    private void generateFloor()
+    {
+        GameObject floor = GameObject.Instantiate(prefab);
+        floor.transform.position = new Vector3(x, y, z);
+        FloorMovement script = floor.GetComponent<FloorMovement>();
+        script.enabled = false;
+        Rigidbody rigidbody = floor.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.back * 10;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            GameObject floor = other.gameObject;
+            FloorMovement script = floor.GetComponent<FloorMovement>();
+            script.enabled = true;
+            generateFloor();
+        }
+    }
+}
