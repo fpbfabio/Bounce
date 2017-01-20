01fc9503424675925563d57d3a0c7c8d372ab44b
Fabio Benigno <benigno.fabio@gmail.com>
2017-01-20 10:39:39 -0400

    Add end game
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

---
+++ Bounce/Assets/Scripts/EndGameScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndGameScript : MonoBehaviour {
+    private Text score;
+
+    // Use this for initialization
+    void Start()
+    {
+        score = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+            PlayerPrefs.SetString("Score", score.text);
+            SceneManager.LoadScene(2);
+        }
+    }
+}
